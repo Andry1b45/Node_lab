955f271dbe9e9db71e8b4b8fed8b88f9ed09559b
Bersh <bersh1b45@gmail.com>
2019-11-23 01:15:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+var port = process.env.PORT || 3002;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
