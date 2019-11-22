39583abc64abd6cdfccc3880674449a11e3a571a
Bersh <bersh1b45@gmail.com>
2019-11-23 01:36:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 const http = require('http');
 const hostname = '127.0.0.1';
-var port = process.env.PORT || 8080;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
