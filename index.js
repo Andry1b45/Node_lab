e26bd9ba3212c062792fdc630871ccca59bf9d20
Bersh <bersh1b45@gmail.com>
2019-11-22 13:16:05 +0200

    1.0.2.2
    simple fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -9,5 +8,5 @@ const server = http.createServer((req, res) => {
     res.end('Hi there!\n');
 });
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running  at http://${hostname}:${port}/`);
 });
