40b28173cdf73abc88742d540989f6b39c9995ee
Bersh <bersh1b45@gmail.com>
2019-11-23 14:22:30 +0200

    minor fix

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 'use strict';
-
-var firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/firestore');
-
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
